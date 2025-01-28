68756fdf667805c44535e2354f8903cfe338418f
Luana Silva <luana.dossantos@zup.com.br>
2025-01-28 16:40:04 -0300

    fix: ajusta tipo Spring para interface

--- src/main/java/com/catalisa/sistema_e_commerce/repository/ClienteRepository.java
+++ src/main/java/com/catalisa/sistema_e_commerce/repository/ClienteRepository.java
@@ -3,5 +3,5 @@ package com.catalisa.sistema_e_commerce.repository;
 import com.catalisa.sistema_e_commerce.model.ClienteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClienteRepository extends JpaRepository<ClienteEntity, Long> {
+public interface ClienteRepository extends JpaRepository<ClienteEntity, String> {
 }
