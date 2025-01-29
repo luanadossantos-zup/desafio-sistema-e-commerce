d54d0ed32257a0c6812868e7711d43656b610c37
Luana Silva <luana.dossantos@zup.com.br>
2025-01-29 15:01:29 -0300

    feat: adiciona criarCliente a ClienteController

--- src/main/java/com/catalisa/sistema_e_commerce/controllers/ClienteController.java
+++ src/main/java/com/catalisa/sistema_e_commerce/controllers/ClienteController.java
@@ -1,4 +1,28 @@
 package com.catalisa.sistema_e_commerce.controllers;
 
+import com.catalisa.sistema_e_commerce.models.Cliente;
+import com.catalisa.sistema_e_commerce.models.Produto;
+import com.catalisa.sistema_e_commerce.services.ClienteService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/clientes")
 public class ClienteController {
+
+
+    private final ClienteService service;
+
+    public ClienteController(ClienteService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<Cliente> criarCliente(@Valid @RequestBody Cliente cliente) {
+        return ResponseEntity.ok(service.criarCliente(cliente));
+    }
 }
