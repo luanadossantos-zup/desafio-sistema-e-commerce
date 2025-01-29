2eebc91ac5ea0b80ba26c8372425320db629d6fd
Luana Silva <luana.dossantos@zup.com.br>
2025-01-29 14:52:41 -0300

    feat: adiciona métodos atualizar,excluir e listar Clientes

---
+++ src/main/java/com/catalisa/sistema_e_commerce/controllers/ClienteController.java
@@ -0,0 +1,4 @@
+package com.catalisa.sistema_e_commerce.controllers;
+
+public class ClienteController {
+}
--- src/main/java/com/catalisa/sistema_e_commerce/services/ClienteService.java
+++ src/main/java/com/catalisa/sistema_e_commerce/services/ClienteService.java
@@ -7,11 +7,15 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class ClienteService {
 
     @Autowired
     private ClienteRepository clienteRepository;
+    private final String CLIENTE_SERVICE = "ClienteService:: ";
 
     public Cliente criarCliente(@Valid Cliente cliente) {
         ClienteEntity entidade = new ClienteEntity(cliente.nome(), cliente.cpf(), cliente.email());
@@ -23,7 +27,40 @@ public class ClienteService {
             return new Cliente(entidadeSalva.getNome(), entidadeSalva.getCpf(), entidadeSalva.getEmail());
         }
 
-
     }
+
+    public Cliente atualizarCliente (@Valid String cpfId,Cliente clienteAtualizado) {
+
+        ClienteEntity entidadeExistente = clienteRepository.findById(cpfId)
+                .orElseThrow(() -> new RuntimeException(CLIENTE_SERVICE + "produto com o nome " + cpfId + " não foi encontrado."));
+
+        entidadeExistente.setNome(clienteAtualizado.nome());
+        entidadeExistente.setCpf(clienteAtualizado.cpf());
+        entidadeExistente.setEmail(clienteAtualizado.email());
+
+        ClienteEntity entidadeSalva = clienteRepository.save(entidadeExistente);
+
+        return new Cliente(entidadeSalva.getNome(), entidadeSalva.getCpf(), entidadeSalva.getEmail());
+    }
+
+    public void excluirCliente(String cpfId) {
+
+        boolean entidadeExiste = clienteRepository.existsById(cpfId);
+
+        if (entidadeExiste) {
+            clienteRepository.deleteById(cpfId);
+        } else {
+            System.out.println(CLIENTE_SERVICE + "cliente com o cpf " + cpfId + " não foi encontrado.");
+            throw new RuntimeException("Cliente com o cpf " + cpfId + " não foi encontrado.");
+        }
+    }
+
+    public List<Cliente> listarTodosClientes() {
+        return clienteRepository.findAll().stream()
+                .map(entity -> new Cliente(entity.getNome(), entity.getCpf(), entity.getEmail()))
+                .collect(Collectors.toList());
+    }
+
+
 
 }
