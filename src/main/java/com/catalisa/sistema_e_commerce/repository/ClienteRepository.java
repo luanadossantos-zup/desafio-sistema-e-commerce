81555319896c7db00bc84654b2226ee48020a1cb
Luana Silva <luana.dossantos@zup.com.br>
2025-01-29 17:22:08 -0300

    feat: adiciona método existsByEmail

--- src/main/java/com/catalisa/sistema_e_commerce/repository/ClienteRepository.java
+++ src/main/java/com/catalisa/sistema_e_commerce/repository/ClienteRepository.java
@@ -4,4 +4,6 @@ import com.catalisa.sistema_e_commerce.models.ClienteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClienteRepository extends JpaRepository<ClienteEntity, String> {
+
+    boolean existsByEmail(String email);
 }
