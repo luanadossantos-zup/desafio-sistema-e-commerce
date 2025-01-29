40f8dc46908a15e6b9e0d29734fa081e34e9c290
Luana Silva <luana.dossantos@zup.com.br>
2025-01-29 16:48:01 -0300

    feat: adiciona handler para TransactionSystemException

--- src/main/java/com/catalisa/sistema_e_commerce/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/catalisa/sistema_e_commerce/exceptions/GlobalExceptionHandler.java
@@ -1,9 +1,15 @@
 package com.catalisa.sistema_e_commerce.exceptions;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.TransactionSystemException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -11,5 +17,21 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(TransactionSystemException.class)
+    public ResponseEntity<Map<String, String>> handleTransactionSystemException(TransactionSystemException ex) {
+        Throwable cause = ex.getRootCause();
+        if (cause instanceof ConstraintViolationException constraintViolationException) {
+            Map<String, String> errors = constraintViolationException.getConstraintViolations()
+                    .stream()
+                    .collect(Collectors.toMap(
+                            violation ->
+                                    violation.getPropertyPath().toString(),
+                                    ConstraintViolation::getMessage
+                    ));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("error", "Erro interno no servidor"));
     }
 }
