d371f32991ea79e47795f9ad531a18704a633ebe
Luana Silva <luana.dossantos@zup.com.br>
2025-01-29 10:35:41 -0300

    feat: adiciona validações as variasveis de instância

--- src/main/java/com/catalisa/sistema_e_commerce/model/ProdutoEntity.java
+++ src/main/java/com/catalisa/sistema_e_commerce/model/ProdutoEntity.java
@@ -1,13 +1,25 @@
 package com.catalisa.sistema_e_commerce.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.data.annotation.Id;
 
 @Entity
 public class ProdutoEntity {
 
 
+    @Id
+    @NotNull(message = "O nome não pode estar em branco!")
+    @Column(unique = true)
     private String nome;
+
+    @NotNull(message = "O preço não pode estar em branco!")
+    @DecimalMin(value = "0.1", message = "O preço deve ser maior que 0!")
     private Double preco;
+
+    @NotNull(message = "A quantidade não pode estar em branco!")
     private int quantidade;
 
     public ProdutoEntity() {
---
+++ src/main/java/com/catalisa/sistema_e_commerce/repository/ProdutoRepository.java
@@ -0,0 +1,4 @@
+package com.catalisa.sistema_e_commerce.repository;
+
+public interface ProdutoRepository {
+}
