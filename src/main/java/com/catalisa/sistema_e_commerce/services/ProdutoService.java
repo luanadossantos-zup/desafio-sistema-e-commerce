a6ad53094f0cf944d56aaeaded93a8f0410053e5
Luana Silva <luana.dossantos@zup.com.br>
2025-01-29 10:51:59 -0300

    feat: adiciona método criarProduto

--- src/main/java/com/catalisa/sistema_e_commerce/services/ProdutoService.java
+++ src/main/java/com/catalisa/sistema_e_commerce/services/ProdutoService.java
@@ -1,6 +1,9 @@
 package com.catalisa.sistema_e_commerce.services;
 
+import com.catalisa.sistema_e_commerce.model.Produto;
+import com.catalisa.sistema_e_commerce.model.ProdutoEntity;
 import com.catalisa.sistema_e_commerce.repository.ProdutoRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,5 +13,11 @@ public class ProdutoService {
     @Autowired
     private ProdutoRepository produtoRepository;
 
+
+    public Produto criarProduto(@Valid Produto produto) {
+        ProdutoEntity entity = new ProdutoEntity(produto.nome(), produto.preco(), produto.quantidade());
+        ProdutoEntity savedEntity = produtoRepository.save(entity);
+        return new Produto(savedEntity.getNome(), savedEntity.getPreco(), savedEntity.getQuantidade());
+    }
 
 }
