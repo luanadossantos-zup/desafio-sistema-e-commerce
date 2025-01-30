b360fe8f9c5a7d0d622c2b8ef31e8f788d5c3236
Luana Silva <luana.dossantos@zup.com.br>
2025-01-30 17:17:45 -0300

    feat: adiciona ComprasController

--- src/main/java/com/catalisa/sistema_e_commerce/controllers/ComprasController.java
+++ src/main/java/com/catalisa/sistema_e_commerce/controllers/ComprasController.java
@@ -1,4 +1,27 @@
 package com.catalisa.sistema_e_commerce.controllers;
 
+import com.catalisa.sistema_e_commerce.models.Compras;
+import com.catalisa.sistema_e_commerce.services.ComprasService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/compras")
 public class ComprasController {
+
+    private final ComprasService service;
+
+    public ComprasController(ComprasService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<Compras> salvarCompras(@Valid @RequestBody Compras compras) {
+
+        return ResponseEntity.ok(service.fazCompra(compras));
+    }
 }
