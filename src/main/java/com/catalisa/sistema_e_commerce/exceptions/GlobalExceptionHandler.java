0d02aa7a9ea0f010bf7b9d50cbd2c92f6e69d021
Luana Silva <luana.dossantos@zup.com.br>
2025-01-31 11:12:01 -0300

    feat: ajusta o Http Status de RuntimeException

--- src/main/java/com/catalisa/sistema_e_commerce/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/catalisa/sistema_e_commerce/exceptions/GlobalExceptionHandler.java
@@ -16,7 +16,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
     @ExceptionHandler(TransactionSystemException.class)
---
+++ src/main/java/com/catalisa/sistema_e_commerce/models/ProdutosComprasEntity.java
@@ -0,0 +1,4 @@
+package com.catalisa.sistema_e_commerce.models;
+
+public class ProdutoCompraEntity {
+}
