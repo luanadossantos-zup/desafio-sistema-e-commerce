5a14da13369ad7e892cbe9a154aea25be1c57b8a
Luana Silva <luana.dossantos@zup.com.br>
2025-02-03 11:35:27 -0300

    feat: adiciona validação de json

--- src/main/java/com/catalisa/sistema_e_commerce/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/catalisa/sistema_e_commerce/exceptions/GlobalExceptionHandler.java
@@ -1,9 +1,13 @@
 package com.catalisa.sistema_e_commerce.exceptions;
 
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.transaction.TransactionSystemException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -34,5 +38,19 @@ public class GlobalExceptionHandler {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
         }
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("error", "Erro interno no servidor"));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Map<String, String>> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        Throwable cause = ex.getCause();
+        if (cause instanceof InvalidFormatException invalidFormatException) {
+            String fieldName = invalidFormatException.getPath().stream().map(JsonMappingException.Reference::getFieldName).findFirst().orElse("desconhecido");
+            String message = String.format("O campo '%s' possui um valor inválido: %s", fieldName, invalidFormatException.getValue());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", message));
+        } else if (cause instanceof MismatchedInputException mismatchedInputException) {
+            String message = "Erro no formato do JSON enviado. Verifique os campos e valores.";
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", message));
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", "Erro ao processar o JSON enviado."));
     }
 }
